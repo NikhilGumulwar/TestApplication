cdcacb0e1c925c11ac998ebaf89cd11de11b236a
MOTIFWORKS\Nikhil.gumulwar <nikhil.gumulwar@motifworks.com>
2016-09-14 16:11:16 +0530

    Initial Commet

---
+++ source/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; 
+namespace HelloGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Display Hello Git Hub
+            Console.WriteLine("Hello Git Hub");
+            // Keep the console window open in debug mode.
+            //Enterkey to exit
+
+            //Console.WriteLine("Name is:-" + p.Name);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            //jgkj
+        }
+    }
+}
+
